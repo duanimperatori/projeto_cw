df9b631117880231533882340d9b4d6a73eeb820
Duan Imperatori <duan.imperatori@projjetta.com>
2024-02-22 22:08:52 -0300

    commit

--- notebook/lib.py
+++ notebook/lib.py
@@ -4,10 +4,11 @@ from sqlalchemy import create_engine
 
 
 def load_csv(file_name):
+    # Function to load a CSV file into a DataFrame.
     df = pd.read_csv(f'../data/csv/{file_name}.csv')
     return df
 
-
+# parameter of the database
 db_params = {
     'host': 'localhost',
     'database': 'db_cloud',
@@ -18,12 +19,11 @@ db_params = {
 
 
 def write_to_dw(table_name, df):
-    
+    # The function receives a DataFrame and the name of the table, and it writes the data into a PostgreSQL database.
     engine = create_engine(f'postgresql+psycopg2://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}:{db_params["port"]}/{db_params["database"]}')
 
     try:
         df.to_sql(table_name, engine, schema='public', if_exists='replace', index=False)
-
         print(f"{table_name} written successfully!")
 
     except Exception as e:
@@ -34,7 +34,7 @@ def write_to_dw(table_name, df):
 
 
 def read_from_dw(table_name: str, columns: list):
-
+    # The function reads data from the database; just provide the name of the table and the columns that must be returned.
     conn = psycopg2.connect(
         host=db_params['host'],
         user=db_params['user'],
@@ -65,7 +65,7 @@ def read_from_dw(table_name: str, columns: list):
 
 
 def read_from_dw_sql(query):
-
+    # The function reads data from the database based on a specified query.
     conn = psycopg2.connect(
         host=db_params['host'],
         user=db_params['user'],
@@ -79,7 +79,7 @@ def read_from_dw_sql(query):
     # Execute the query
     cur.execute(query)
 
-    # Fetch all the rows
+    # Fetch all the rows and get the name of the columns
     rows = cur.fetchall()
     columns = [desc[0] for desc in cur.description]
 
@@ -94,7 +94,7 @@ def read_from_dw_sql(query):
 
 
 def create_primary_key(table_name, primary_key):
-
+    # The functions creates a primary key in the table.
     conn = psycopg2.connect(
         host=db_params['host'],
         user=db_params['user'],
